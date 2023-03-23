e00db4a588f575147b59a277d8bba720b59f0f4a
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-23 18:43:05 +0530

    [Chandra]

---
+++ Generic_Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Code
+{
+    internal class Generic_Class<G>
+    {
+        public static void DeleteElement(G[] arr,  G delete)
+        {
+            int k = 0;
+            G[] result = new G[arr.Length - 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (delete.Equals(arr[i]))
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+
+                foreach (G j in result)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+
+        }
+    }
+}
