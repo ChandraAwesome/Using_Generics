acc0b6a59f4f9f29b745ef8317ea747ade8599fb
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-23 18:13:08 +0530

    [Chandra]

---
+++ DeleteElementss.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    public class DeleteElementss
+
+    {
+        public static void DeleteElement(int[] arr, int delete)
+        {
+            int k = 0;
+            int[] result = new int[arr.Length - 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (delete != arr[i])
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+
+                foreach (int j in result)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+
+        }
+        public static void DeleteElement1(double[] arr, double delete)
+        {
+            int k = 0;
+            double[] result = new double[arr.Length - 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (delete != arr[i])
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+
+                foreach (double j in result)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+        }
+
+        public static void DeleteElement2(char[] arr, char delete)
+        {
+            int k = 0;
+            char[] result = new char[arr.Length - 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (delete != arr[i])
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+
+                foreach (int j in result)
+                {
+                    Console.Write(j + " ");
+                }
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,77 @@
-﻿namespace Generics
+﻿using Patterns._4B_programs;
+
+namespace Patterns
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, Generics!");
-            FindMaximumint.MaximumNum(20,30,40);
-            FindMaximum_double.MaximumNum(20.5, 52.3,46.3);
-            FindMaximum_string.MaximumNum("Ajay", "Chandra", "Awesome");
+            //Access_Modifiers access= new Access_Modifiers();
+            //Console.WriteLine(access.y); // access the public modifier data
+            //Console.WriteLine(access.z);// we got error coz of z is a protected data type
+            //Console.WriteLine(access.x);// x is an private data type [error]
+            //
+            // Console.WriteLine("Hello, World!");
+            //Triangle.ReverseAngleTriangle();
+            //Triangle.RightAngleTriangle();
+            //Triangle.EquiTringle();
+            //Triangle.ReverseEqiTriangle();
+            //ReverseaNumber.ReverseusingChar();
+            //ReverseaNumber.Reverseusingloop();
 
+            //SumofGivenNumbers.Add();
+            //SumofGivenNumbers.UsingArray();
+
+            //Duplicate_Elements.Count();
+            // MaxandMin.MaximumandMinimum();
+
+          //  ADDING AND DELETING ELEMENTS IN ARRAY USING INT DATA TYPE
+           // Console.WriteLine("Add the no of elements");
+           // int size = Convert.ToInt32(Console.ReadLine());
+           // int[] arr = new int[size];
+           //// int[] arr = { 10, 20, 30 };
+           // Console.WriteLine("Enter the elements");
+           // Console.WriteLine("Select from above Elements ");
+           // foreach (int j in arr)
+           // {
+           //     arr[j] = Convert.ToInt32(Console.ReadLine());
+           //     Console.WriteLine(j + " ");
+           // }
+           // Console.WriteLine("\n Plese pickup one element to delete");
+           // int delte = Convert.ToInt32(Console.ReadLine());
+           // DeleteElementss.DeleteElement(arr,delte);
+
+            //  //------------------USING THE DOUBLE DATA TYPE ELEMENTS----------------------------//
+
+            //  Console.WriteLine("Add the no of elements");
+            //  int sizee = Convert.ToInt32(Console.ReadLine());
+            //  int[] arrr = new int[sizee];
+            //  //int[] arr = {10,20, 30};
+            //  Console.WriteLine("Enter the elements");
+            //  //  Console.WriteLine("Select from above Elements ");
+            //  foreach (int j in arrr)
+            //  {
+            //      arr[j] = Convert.ToInt32(Console.ReadLine());
+            //      //Console.WriteLine(j+" ");
+            //  }
+            //  Console.WriteLine("\n Plese pickup one element to delete");
+            //  int delete = Convert.ToInt32(Console.ReadLine());
+             // DeleteElementss.DeleteElement1(arr, delete);
+
+            Console.WriteLine("Add the no of elements");
+            int sizee = Convert.ToInt32(Console.ReadLine());
+            char[] arrr1 = new char[sizee];
+            //int[] arr = {10,20, 30};
+            Console.WriteLine("Enter the elements");
+            //  Console.WriteLine("Select from above Elements ");
+            foreach (int j in arrr1)
+            {
+                arrr1[j] = Convert.ToChar(Console.ReadLine());
+                //Console.WriteLine(j+" ");
+            }
+            Console.WriteLine("\n Plese pickup one element to delete");
+            char delete = Convert.ToChar(Console.ReadLine());
+            DeleteElementss.DeleteElement2(arrr1, delete);
         }
     }
 }
