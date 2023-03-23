23df50c476db119672c79c34de7f9864d83e947b
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-23 18:11:18 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, Generics!");
+            FindMaximumint.MaximumNum(20,30,40);
+            FindMaximum_double.MaximumNum(20.5, 52.3,46.3);
+            FindMaximum_string.MaximumNum("Ajay", "Chandra", "Awesome");
+
+        }
+    }
+}
